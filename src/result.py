87dcc2d52c493f1380787a549fb47374e836e55c
Daniel Bradburn <moagstar@gmail.com>
2021-10-08 06:56:07 +0200

    Refactor to make it easier to do manual processing

--- src/result.py
+++ src/result.py
@@ -1,8 +1,10 @@
 import json
 import re
 import sys
-from datetime import datetime
+import datetime
+import typing
 from pathlib import Path
+from typing import Tuple, List, Any, Dict
 
 import splinter
 from funcoperators import to, filterwith
@@ -12,42 +14,86 @@ from toolz import first, keymap, valmap, groupby, count
 DIR = Path(__file__).parent
 
 
-if __name__ == "__main__":
+class Teams(typing.TypedDict):
+    Red: List[str]
+    Blue: List[str]
 
-    winner = sys.argv[1]
-    date = sys.argv[2] | to(int) if len(sys.argv) > 2 else 0
 
-    # region Get teams / Event date
-    executable_path = DIR / '../bin/chromedriver'
-    with splinter.Browser('chrome', executable_path=executable_path) as browser:
-        browser.visit("https://www.meetup.com/Ijburg-Futsal-Regulars/events/past/")
-        event = (browser.find_by_css('.eventCard--link') | to(list))[date]
-        browser.visit(event['href'])
-        date = browser.find_by_tag('time')['datetime'][:-3] | to(int)  # strip milliseconds
-        for comment in browser.find_by_css('.comment-content'):
-            if 'Teams for' in comment.text:
-                teams = comment.text
-                break
-        else:
-            raise ValueError('No teams comment found')
+def get_teams(event_comment: str) -> Teams:
+    """
+    Parse the event comment to figure out who was playing on which team.
 
+    :param event_comment: The comment from meetup saying what the teams are.
+
+    :return: Teams dict with 'Red' and 'Blue' keys, the values are lists of
+             player names.
+    """
     pattern = re.compile('\s*(red|blue):([^$]*)$', flags=re.IGNORECASE)
     teams = dict(
         pattern.findall(line) | to(list) | to(first)
-        for line in teams.split('\n')
+        for line in event_comment.split('\n')
         if pattern.match(line)
     )
     teams = keymap(str.title, teams)
     teams = valmap(lambda x: [y.strip() for y in x.split(',')], teams)
-    date = datetime.utcfromtimestamp(date).date()
-    # endregion
 
-    # region Update matches
+    return teams
+
+
+def get_event(offset: int) -> Tuple[datetime.date, str]:
+    """
+    Scrape the event from meetup comments.
+
+    :param offset: Which event to get, i.e. 0 == this week, -1 last week etc.
+
+    :return: Tuple of the date of the event and the text of the comment
+             containing the teams.
+    """
+    executable_path = DIR / '../bin/chromedriver'
+    with splinter.Browser('chrome', executable_path=executable_path) as browser:
+        browser.visit("https://www.meetup.com/Ijburg-Futsal-Regulars/events/past/")
+        event_el = (browser.find_by_css('.eventCard--link') | to(list))[offset]
+        browser.visit(event_el['href'])
+        date = browser.find_by_tag('time')['datetime'][:-3] | to(int)  # strip milliseconds
+        for comment in browser.find_by_css('.comment-content'):
+            if 'Teams for' in comment.text:
+                event = comment.text
+                break
+        else:
+            raise ValueError('No teams comment found')
+    date = datetime.datetime.utcfromtimestamp(date).date()
+
+    return date, event
+
+
+class PlayerMatch(typing.TypedDict):
+    date: datetime.date
+    name: str
+    team: str
+    points: int
+
+
+def update_matches(date: datetime.date, teams: Teams, winner: typing.Literal['r', 'b', 'd']) -> List[PlayerMatch]:
+    """
+    Update the matches json file containing a history of the matches that were
+    played. If the match for a particular date already exists those values are
+    overwritten with the new values.
+
+    :param date: Date that the match took place.
+    :param teams: Which players played on which colour team.
+    :param winner: Which team was the winner (or draw)
+
+    :return: The records from all matches that have been played.
+    """
     winner = {'b': 'Blue', 'r': 'Red', 'd': 'Draw'}[winner]
+
     with open(DIR/'../data/matches.json') as f:
         matches = json.load(f)
         matches_by_date = groupby(lambda x: x['date'], matches | to(list))
-        matches_by_date = keymap(lambda x: datetime.strptime(x, '%Y-%m-%d').date(), matches_by_date)
+        matches_by_date = keymap(
+            lambda x: datetime.datetime.strptime(x, '%Y-%m-%d').date(),
+            matches_by_date,
+        )
 
     matches_by_date[date] = [
         {
@@ -61,11 +107,16 @@ if __name__ == "__main__":
     ]
 
     with open(DIR/'../data/matches.json', 'w') as f:
-        records = [match for date, matches in matches_by_date.items() for match in matches]
+        matches = [match for date, matches in matches_by_date.items() for match in matches]
         json.dump(sorted(records, key=lambda x: x['date'], reverse=True), f, indent=2)
-    # endregion
 
-    # region Update stats
+    return matches
+
+
+def update_stats(matches: List[PlayerMatch]):
+    """
+    Update the statistics file based on the played matches.
+    """
     with open(DIR/'../data/stats.json', 'w') as f:
         stats = [
             {
@@ -82,8 +133,21 @@ if __name__ == "__main__":
                     1,
                 ),
             }
-            for name, games in groupby(lambda x: x['name'], records).items()
+            for name, games in groupby(lambda x: x['name'], matches).items()
         ]
         json.dump(sorted(stats, key=lambda x: x['pointsPerGame'], reverse=True), f, indent=2)
-    # endregion
 
+
+if __name__ == "__main__":
+
+    winner = sys.argv[1]
+    offset = sys.argv[2] | to(int) if len(sys.argv) > 2 else 0
+    date, event_comment = get_event(offset=offset)
+    
+    event_comment = """
+        
+    """; date = datetime.date(2021, 9, 1)
+
+    teams = get_teams(event_comment)
+    records = update_matches(date, teams, winner)
+    update_stats(records)
